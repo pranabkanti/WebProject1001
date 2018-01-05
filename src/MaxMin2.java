335ebf05fb6588c6fa32761dd87a48adf9675539
pranabkanti <32228437+pranabkanti@users.noreply.github.com>
2018-01-05 15:01:30 -0500

    new program added

---
+++ src/MaxMin2.java
@@ -0,0 +1,39 @@
+// Pranab k nath
+// this is a modified program to give some freedom to the user to enter 
+// the elements of the array after running the program which is a kind of 
+// user interface program
+
+import java.lang.Integer;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class MaxMin2 {
+	
+	public static void main(String[] args) {
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		Scanner scan = new Scanner(System.in);
+	    System.out.println("enter the numbers one by one and enter any non-Integer to finish");
+	    while (scan.hasNextInt())
+	    {  	
+	      numbers.add(scan.nextInt());
+	    }   
+		
+		
+		System.out.println("number you entered is: "+numbers);
+		Integer[] arr = numbers.toArray(new Integer[numbers.size()]);
+		int max = 0; //or int max = numbers[0];
+		int minima = arr[0];
+		for (int i=0;i<arr.length;i++)
+		{
+			
+			if(max<arr[i])
+				max=arr[i];
+			if(arr[i]<minima)
+				minima = arr[i];
+		}
+System.out.println("max number is: "+max);
+System.out.println("max number is: "+minima);
+	}
+
+	}
